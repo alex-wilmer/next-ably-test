a91e22f0ff60a55733afda9db3df296b51cc5310
Alex Wilmer <djazium@gmail.com>
2022-08-03 19:31:33 -0400

    wording no quotes

--- pages/login.js
+++ pages/login.js
@@ -46,7 +46,7 @@ export default function Login() {
               display: 'block',
             }}
           >
-            4 Digit Pin (Don't forget it!)
+            4 Digit Pin (Remember this!)
           </label>
           <PinInput
             length={4}
