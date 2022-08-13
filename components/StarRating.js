68436e4d417bd52f4ef97d57fa6759815d7f725b
Alex Wilmer <djazium@gmail.com>
2022-08-13 11:46:02 -0400

    fix star rating icons

--- components/StarRating.js
+++ components/StarRating.js
@@ -14,7 +14,9 @@ export default function StarRating({ rate }) {
         id={`star1`}
         onClick={() => rate(5)}
       />
-      <label className="full" htmlFor={`star1`} />
+      <label className="full" htmlFor={`star1`}>
+        ★
+      </label>
       <input
         className="star"
         type="radio"
@@ -22,7 +24,9 @@ export default function StarRating({ rate }) {
         id={`star2`}
         onClick={() => rate(4)}
       />
-      <label className="full" htmlFor={`star2`} />
+      <label className="full" htmlFor={`star2`}>
+        ★
+      </label>
       <input
         className="star"
         type="radio"
@@ -30,7 +34,9 @@ export default function StarRating({ rate }) {
         id={`star3`}
         onClick={() => rate(3)}
       />
-      <label className="full" htmlFor={`star3`} />
+      <label className="full" htmlFor={`star3`}>
+        ★
+      </label>
       <input
         className="star"
         type="radio"
@@ -38,7 +44,9 @@ export default function StarRating({ rate }) {
         id={`star4`}
         onClick={() => rate(2)}
       />
-      <label className="full" htmlFor={`star4`} />
+      <label className="full" htmlFor={`star4`}>
+        ★
+      </label>
       <input
         className="star"
         type="radio"
@@ -46,7 +54,9 @@ export default function StarRating({ rate }) {
         id={`star5`}
         onClick={() => rate(1)}
       />
-      <label className="full" htmlFor={`star5`} />
+      <label className="full" htmlFor={`star5`}>
+        ★
+      </label>
     </fieldset>
   )
 }
